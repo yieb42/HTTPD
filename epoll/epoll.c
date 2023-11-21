de569193bde988a82e958306fea2f07efb9f0ceb
yahya ilyas EL BERNOUSSI <yahya-ilyas.el-bernoussi@epita.fr>
2023-11-21 14:09:28 +0100

    changement create

--- epoll/epoll.c
+++ epoll/epoll.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <sys/epoll.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/epoll.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[])
@@ -16,12 +16,13 @@ int main(int argc, char *argv[])
     struct epoll_event events[100];
 
     int pipefd = open(argv[1], O_RDONLY);
-    if (pipefd == -1) {
+    if (pipefd == -1)
+    {
         perror("open");
         return 1;
     }
     int nfds;
-    int epollfd = epoll_create(4);
+    int epollfd = epoll_create1(0);
     if (epollfd == -1)
     {
         perror("epoll create");
@@ -30,7 +31,7 @@ int main(int argc, char *argv[])
 
     ev.events = EPOLLIN | EPOLLET;
     ev.data.fd = pipefd;
-    if (epoll_ctl(epollfd,EPOLL_CTL_ADD,pipefd,&ev) == -1)
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, pipefd, &ev) == -1)
     {
         perror("epoll ctl for epollfd");
         return 1;
@@ -38,7 +39,7 @@ int main(int argc, char *argv[])
     char buff[32] = { 0 };
     while (1)
     {
-        nfds = epoll_wait(epollfd,events,100,-1);
+        nfds = epoll_wait(epollfd, events, 100, -1);
         if (nfds == -1)
         {
             perror("epoll wait");
@@ -48,7 +49,7 @@ int main(int argc, char *argv[])
         {
             if (events[i].events & EPOLLIN)
             {
-                ssize_t readd = read(events[i].data.fd,buff,32);
+                ssize_t readd = read(events[i].data.fd, buff, 32);
                 if (readd == -1)
                 {
                     perror("error on read");
@@ -57,17 +58,19 @@ int main(int argc, char *argv[])
 
                 buff[readd] = '\0';
 
-                if (strcmp(buff,"pong") == 0)
+                if (strcmp(buff, "pong") == 0)
                 {
                     printf("ping!\n");
                 }
-                else if (strcmp(buff,"ping") == 0)
+                else if (strcmp(buff, "ping") == 0)
                 {
                     printf("pong!\n");
                 }
-                else if (strcmp(buff,"quit") == 0)
+                else if (strcmp(buff, "quit") == 0)
                 {
                     printf("quit\n");
+                    close(pipefd);
+                    close(epollfd);
                     return 0;
                 }
                 else
